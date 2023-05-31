6a3485915c087500f835bd861245e706a334d9aa
JeongJunui <102797089+JeongJunui@users.noreply.github.com>
2023-05-31 11:46:01 +0900

    직접 수정

--- demo/src/main/java/com/example/movieProject/module/board/entity/QnaEntity.java
+++ demo/src/main/java/com/example/movieProject/module/board/entity/QnaEntity.java
@@ -1,8 +1,5 @@
-<<<<<<<< HEAD:demo/src/main/java/com/example/movieProject/module/board/AgoraDTO.java
-package com.example.movieProject.module.board;
-========
+
 package com.example.movieProject.module.board.entity;
->>>>>>>> 0b361bf7f6446238e5bfe82dd3a4894702cb5a7c:demo/src/main/java/com/example/movieProject/module/board/entity/QnaEntity.java
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
