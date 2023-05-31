acf436c451a22892d8dfd7448912ba2f8cc8cf28
JeongJunui <102797089+JeongJunui@users.noreply.github.com>
2023-05-31 11:45:30 +0900

    직접 수정

--- demo/src/main/java/com/example/movieProject/module/board/entity/BoardLikeEntity.java
+++ demo/src/main/java/com/example/movieProject/module/board/entity/BoardLikeEntity.java
@@ -1,8 +1,6 @@
-<<<<<<<< HEAD:demo/src/main/java/com/example/movieProject/module/board/BoardLikeDTO.java
-package com.example.movieProject.module.board;
-========
+
 package com.example.movieProject.module.board.entity;
->>>>>>>> 0b361bf7f6446238e5bfe82dd3a4894702cb5a7c:demo/src/main/java/com/example/movieProject/module/board/entity/BoardLikeEntity.java
+
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
